1c1f13d1dc4602e69860c324dc6dd72020091b34
hemanthreddy-45 <140798143+hemanthreddy-45@users.noreply.github.com>
2024-04-22 11:27:59 +0530

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -1,7 +1,8 @@
 echo "Hello World!"
 AWS_ACCESS_KEY="ASDUHAEUR73923MSA9"
 AWS_SECRET_ACCESS_KEY="ASJH8342LKD/AODHQNDAS52"
-password = F@7!qZ#2
-hello = F@7!qZ#2
-token = "P$5aRt#2oN!9lM@8"
+hello = "JDF@12`~F@7!qZ#2"
+password = P$5aRt#2oN!9lM@8
 password = "P$5aRt#2oN!9lM@8"
+token= JP$5aGt#4oN!9lM@8AS
+token= "JP$5aGt#4oN!9lM@8AS"
