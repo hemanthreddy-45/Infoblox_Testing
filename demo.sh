d578a2da03b86efb39435cc19fe4d3810c847e4e
hemanthreddy-45 <140798143+hemanthreddy-45@users.noreply.github.com>
2024-04-22 11:24:05 +0530

    Create demo.sh

---
+++ demo.sh
@@ -0,0 +1,7 @@
+echo "Hello World!"
+AWS_ACCESS_KEY="ASDUHAEUR73923MSA9"
+AWS_SECRET_ACCESS_KEY="ASJH8342LKD/AODHQNDAS52"
+password = F@7!qZ#2
+hello = F@7!qZ#2
+token = "P$5aRt#2oN!9lM@8"
+password = "P$5aRt#2oN!9lM@8"
