a78298230183e7b3b096c4f35253c2d2dc4a980f
hemanthreddy-45 <140798143+hemanthreddy-45@users.noreply.github.com>
2024-08-14 10:53:18 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,3 +2,5 @@ hello = "JDF@12`~F@7!qZ#2"
 password = P$5aRt#2oN!9lM@8
 password = "P$5aRt#2oN!9lM@8"
 token= JP$5aGt#4oN!9lM@8AS
+token = "hem1234$%$**AZkn@145789}|~"
+token= "3DS$5aGt#4oN!9lM@8AS"
